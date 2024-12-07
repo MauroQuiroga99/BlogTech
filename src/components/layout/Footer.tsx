22508985ebbc36d04a96b7865fee9536382cdacd
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-12-07 10:39:22 -0500

    Crear Header y Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="h-40 bg-gray-900 fixed bottom-0 w-full flex justify-center ">
+        <div className="flex  w-[1000px] items-center gap-8  ">
+          <div className="flex justify-start gap-8 ">
+            <div className="text-base tracking-wider leading-4 font-medium text-gray-300 not-italic hover:text-blue-800 transition-colors">
+              Algún texto{" "}
+            </div>
+            <div className="text-base tracking-wider leading-4 font-medium text-gray-300 not-italic hover:text-blue-800 transition-colors">
+              Algún texto{" "}
+            </div>
+            <div className="text-base tracking-wider leading-4 font-medium text-gray-300 not-italic hover:text-blue-800 transition-colors">
+              Algún texto{" "}
+            </div>
+          </div>
+          <div className="flex-col">
+            <h1 className="text-lg text-gray-300 font-bold">BLOGTECH</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,146 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const menuRef = useRef<HTMLUListElement | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div>
+      <nav className="h-[90px] p-5 flex justify-center bg-white">
+        <div className="flex w-[1000px] justify-between items-center">
+          <h1 className="text-[26px] font-bold">BLOGTECH</h1>
+          <div className="hidden lg:flex">
+            <ul className="flex gap-6 mx-10 items-center">
+              <li>
+                <a
+                  className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                  href=""
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                  href=""
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                  href=""
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                  href=""
+                >
+                  Blog Feed
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-base px-7 py-3 tracking-wider leading-4 font-bold uppercase text-black not-italic border-2 border-black hover:bg-blue-800 hover:text-white"
+                  href=""
+                >
+                  Sign In
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="relative flex-row gap-4 lg:hidden">
+            <button
+              onClick={toggleMenu}
+              ref={buttonRef}
+              className="mr-4 text-2xl w-7"
+            >
+              <img src="/icon/nav-bar.svg" alt="" />
+            </button>
+
+            {menuOpen && (
+              <ul
+                ref={menuRef}
+                className="fixed top-0 left-0 z-50 w-44 flex flex-col gap-6 items-start bg-white p-5 shadow-md"
+              >
+                <li>
+                  <a
+                    className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                    href=""
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                    href=""
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                    href=""
+                  >
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-base tracking-wider leading-4 font-medium text-gray-800 not-italic hover:text-blue-800 transition-colors"
+                    href=""
+                  >
+                    Blog Feed
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-base px-7 py-3 tracking-wider leading-4 font-bold uppercase text-black not-italic border-2 border-black hover:bg-blue-800 hover:text-white"
+                    href=""
+                  >
+                    Sign In
+                  </a>
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
